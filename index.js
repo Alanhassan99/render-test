be3b6fcd7917dcf0e11595376a45459ef7f2cc5e
Alan Hassan <alan@MacBook-Air-som-tillhor-Alan.local>
2026-06-10 11:43:33 +0200

    fix mongodb connection and routes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require('dotenv').config()
 const Person = require('./models/person')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.static('dist'))
-
 app.use(express.json())
-const morgan = require('morgan')
-
 morgan.token('helu', function (req, res) {
     return JSON.stringify(req.body)
 })
-
 app.use(morgan(function (tokens, req, res) {
     return [
         tokens.method(req, res),
@@ -30,9 +27,6 @@ app.get('/info', (request, response) => {
         `)
 })
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => response.json(persons))
-})
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
@@ -44,6 +38,11 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(persons => response.json(persons))
+        .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
         response.status(204).end()
@@ -53,14 +52,50 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     const person = new Person({
         name: body.name,
         number: body.number
     })
     person.save().then(saved => response.json(saved))
+        .catch(error => next(error))
+
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+    Person.findById(request.params.id).then(person => {
+        if (!person) {
+            return response.status(404).end()
+        }
+        person.name = name
+        person.number = number
+
+        return person.save().then((updatedPerson) => {
+            response.json(updatedPerson)
+        })
+    })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
+
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
